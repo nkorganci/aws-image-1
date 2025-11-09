e9995c32dac104f69178e6a1ba6d70477065dae2
nkorganci <nkorganci@gmail.com>
2025-11-09 07:46:56 -0600

    us-east-1 added

---
+++ terraform/variables.tf
@@ -0,0 +1,56 @@
+// filepath: d:\workspace\aws-image-1\terraform\variables.tf
+
+variable "aws_region" {
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Name prefix for resources"
+  type        = string
+  default     = "aws-image-1"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.0.0/24", "10.0.1.0/24"]
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "app_port" {
+  description = "Port the application listens on"
+  type        = number
+  default     = 8081
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID to use for EC2 instance (set this per-region before apply)"
+  type        = string
+  default     = ""
+}
+
+variable "ssh_user" {
+  description = "SSH user for the chosen AMI (ec2-user, ubuntu, etc.)"
+  type        = string
+  default     = "ec2-user"
+}
+
